7c4e6f80188c6fa9a71bd02a7228e223107a8fa7
jgonz671 <jgonz671@ucr.edu>
2024-02-12 02:53:41 -0800

    full nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,18 @@ import './Navbar.css'
 function NavBar() {
   return (
     <nav>
-      <ul>
-        <li><Link to="/home">Home</Link></li>
-        <li><Link to="/project">Projects</Link></li>
-        <li><Link to="/history">History</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
+      <div className="navbar-container">
+        <ul className="social-links">
+          <li><a href="https://www.linkedin.com/in/julian-gonzalez-b49665203/" target="_blank">LinkedIn</a></li>
+          <li><a href="https://github.com/jgonz671" target="_blank">GitHub</a></li>
+        </ul>
+        <ul className="navigation-links">
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/project">Projects</Link></li>
+          <li><Link to="/history">History</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </div>
     </nav>
   );
 }
--- src/routes/Text.jsx
+++ src/routes/Text.jsx
@@ -8,7 +8,7 @@ function Text(){
           <Typewriter
             onInit={(typewriter) => {
               typewriter
-                .typeString("My name is Julian Gonzalez")
+                .typeString("My name is Julian Gonzalez.")
                 .pauseFor(1000)
                 .start()
             }}
