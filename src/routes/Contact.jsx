33e88c2ce4ad2360acdd5e8a4daf81bbac968476
jgonz671 <jgonz671@ucr.edu>
2024-02-12 12:54:12 -0800

    basic website UI/content

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import '../styles/Contact.css'
 
 function Contact() {
-  return <div>Contact</div>;
-}
+  return (
+    <div className="contact-form">
+      <label htmlFor="phone">Phone Number:</label>
+      <a href="tel:+9513918770">951-391-8770</a>
+      <br>
+      </br>
+      <label htmlFor="email">Email:</label>
+      <a href="mailto:julian.eg2003@gmail.com">julian.eg2003@gmail.com</a>
+    </div>
+  );
 
+  }
 export default Contact;
--- src/routes/Project.jsx
+++ src/routes/Project.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import '../styles/Project.css'; 
 
 function Project() {
-  return <div>Project</div>;
+  return (
+    <div className="project-details">
+      <h2>Rubbish Radar</h2>
+      <p>Developed a web-based application leveraging Vue.js framework focused on facilitating user-located waste disposal points displayed on an interactive map.</p>
+      <p>Hosted the application on Google Firebase for scalable cloud computing capabilities.</p>
+      <p>Integrated Google Maps API to render a custom map interface, featuring color-coded markers that distinguish various types of waste disposals.</p>
+      <p>Introduced an upvote/downvote functionality exclusive to authenticated users to ensure data reliability.</p>
+      <p>Implemented user authentication using Firebase OAuth enhancing user personalization and security.</p>
+
+      <h2>Labyrinth Looter</h2>
+      <p>Terminal-based RPG game in C++ that traverses through a dynamically generated simulated dungeon.</p>
+      <p>Utilized Google Test framework to construct comprehensive test suites for each source file.</p>
+      <p>Employed Valgrind to check for memory leaks and undefined memory usage.</p>
+
+      <h2>Home Security System</h2>
+      <p>Designed home security system on Arduino Uno R3 using ultrasonic sensor for motion detection, IR remote control for wireless disarming, and active buzzer for audible alarms.</p>
+      <p>Optimized C++ code in Arduino IDE to utilize Arduino Uno R3 limited memory and processing capabilities.</p>
+    </div>
+  );
 }
 
 export default Project;
