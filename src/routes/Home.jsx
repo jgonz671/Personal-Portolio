fe4c9dfc0643b9f9c83ab4468b05f1705b0a8213
Julian Gonzalez <Julian@julian.eg2003@gmail.com>
2024-06-10 14:03:23 -0700

    title change

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -13,7 +13,7 @@ function Home(){
               .pauseFor(250)
               .typeString("<br>")
               .pauseFor(250)
-              .typeString("Computer Engineer.")
+              .typeString("Software Engineer.")
               .start()
           }}
         />
